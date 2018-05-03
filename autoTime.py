c1720e724831f0c12839df03eeeb6689974a1e09
dwertent <davhadassah@gmail.com>
2018-05-03 12:16:31 +0300

    jason files

--- autoTime.py
+++ autoTime.py
@@ -1,18 +1,50 @@
 import schedule
-from ps import ps
-import time
-
+import datetime
 import smtplib
+import glob
+import errno
+import json
+import os.path
+from pprint import pprint
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
+from ps import ps
+
+path_to_order_files = 'order_files/'
+path_to_send_to_chef_files = 'send_to_chef_files/'
+
+
+def set_order():
+    path = os.path.join(path_to_order_files, '*.json')
+    files = glob.glob(path)
+    order_data = {}
+    for name in files:
+        try:
+            with open(name, encoding='utf-8') as f:
+                order_data.update(json.load(f))
+
+        except IOError as exc:
+            if exc.errno != errno.EISDIR:
+                raise  # Propagate other kinds of IOError.
+
+    with open(os.path.join(path_to_send_to_chef_files, 'order_lunch_' + str(datetime.date.today())) + '.json',
+              'w') as outfile:
+        json.dump(order_data, outfile)
+
+
+def empty_orders_dir():
+    path = os.path.join(path_to_order_files, '*.json')
+    files = glob.glob(path)
+    for name in files:
+        os.remove(name)
 
 
 def send_email():
     sender_email_address = 'LttsLunch@gmail.com'
     sender_email_password = ps
-    receiver_email_address = 'Amit.Tene@lnttechservices.com'
+    receiver_email_address = 'davw664@gmail.com'
 
     email_subject_line = 'Please read the email!'
 
@@ -25,12 +57,14 @@ def send_email():
     email_body += '\n\n' + "Have A wonderful day!!" + '\n\n' + "David"
     msg.attach(MIMEText(email_body, 'plain'))
 
-    filename = 'text.txt'
+    filename = os.path.join(path_to_send_to_chef_files,
+                            'order_lunch_' + str(datetime.date.today())) + '.json'
     attachment_file = open(filename, 'rb')
     part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment_file).read())
+    part.set_payload(attachment_file.read())
     encoders.encode_base64(part)
-    part.add_header('Content-Disposition', "attachment_file; filename = " + filename)
+    part.add_header('Content-Disposition', "attachment_file; filename = order_lunch_" +
+                    str(datetime.date.today()) + '.json')
     msg.attach(part)
 
     email_body_content = msg.as_string()
@@ -42,7 +76,10 @@ def send_email():
 
 
 if __name__ == '__main__':
-    schedule.every().day.at("10:00").do(send_email)
-    while True:
-        schedule.run_pending()
-
+    set_order()
+    send_email()
+    # empty_orders_dir()
+    # schedule.every().day.at("10:52").do(set_order)
+    # schedule.every().day.at("10:53").do(send_email)
+    # while True:
+    #     schedule.run_pending()
