9a15e1fb9df9c0565cfb0bb963dc97398b7883c1
Amit Tene <atene@cisco.com>
2018-05-03 07:51:16 -0700

    fix issues

--- server.py
+++ server.py
@@ -5,26 +5,20 @@ from common import ORDER_FIELDS
 
 app = Flask(__name__)
 path_to_order_files = 'order_files/'
-
+cibus_number = ORDER_FIELDS[0]
 
 @app.route('/', methods=['POST', 'GET'])
 def ui():
    return render_template('menue.html', order_fields = ORDER_FIELDS)
 
-
-
 @app.route('/get_order', methods=['POST'])
 def get_order():
     order = {}
-    name = request.form['name']
-    cibus = request.form['cibus']
-    mainDish = request.form['maindish']
-    sideDish1 = request.form['side1']
-    sideDish2 = request.form['side2']
+    cibus = request.form[cibus_number]
+    for field in ORDER_FIELDS:
+        order.update({field:request.form[field]})
 
-
-    order.update({'cibus': cibus, 'name': name, 'main_dish': mainDish, 'side_dish_no.1': sideDish1,
-                  'side_dish_no.2': sideDish2})
+    order.update({'notes': request.form['notes']})
     print(order)
     with open(os.path.join(path_to_order_files, '{}.json'.format(cibus)), 'w') as fp:
         json.dump(order, fp, indent=4, sort_keys=True)
