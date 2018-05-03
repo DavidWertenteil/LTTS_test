0878ae9929da7eb88de277e5af7e5a603129f291
Amit Tene <atene@cisco.com>
2018-05-03 04:08:55 -0700

    adding the Flask Server files include all the Bootstrap and other static files

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import json
+
+from Testing import single_test, multi_test
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST', 'GET'])
+def ui():
+   return render_template('menue.html')
+
+
+@app.route('/get_order', methods = ['POST'])
+def get_order():
+        order = {}
+        name = request.form['name']
+        cibus = request.form['cibus']
+        mainDish = request.form['maindish']
+        sideDish1 = request.form['side1']
+        sideDish2 = request.form['side2']
+
+        order[cibus] = {}
+        order[cibus].update({'name': name, 'main_dish': mainDish, 'side_dish_no.1': sideDish1,
+                             'side_dish_no.2': sideDish2})
+        print (order)
+        with open('{}.json'.format(cibus), 'w') as fp:
+            json.dump(order, fp, indent=4, sort_keys=True)
+
+        return render_template('menue.html'), 201
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0')
