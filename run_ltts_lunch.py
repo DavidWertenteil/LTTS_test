c9a7baffd5ad615eba061cd40f6527c93f072be7
dwertent <davhadassah@gmail.com>
2018-05-06 12:25:24 +0300

    run_ltts_lunch testing

--- run_ltts_lunch.py
+++ run_ltts_lunch.py
@@ -2,7 +2,7 @@ from os import system
 
 
 if __name__ == '__main__':
-    system('screen -S backend python3 be_server.py &')
+    system('screen -S backend echo mkdcm')
     system('screen -r backend')
     system('cd backend/')
     system('python3 be_server.py &')
