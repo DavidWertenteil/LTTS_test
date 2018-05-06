7f80d099ee55236f8fce300302cb8a3712b15cb2
dwertent <davhadassah@gmail.com>
2018-05-06 13:44:23 +0300

    run_ltts_lunch testing

--- run_ltts_lunch.py
+++ run_ltts_lunch.py
@@ -1,15 +1,17 @@
 import subprocess, os
 
-run_backend_srver = [['screen', '-S', 'backend'],
-                     ['cd', 'backend/'],
-                     ['python3', 'be_server.py', '&']]
+#
+# run_backend_srver = [['screen', '-S', 'backend'],
+#                      ['cd', 'backend/'],
+#                      ['python3', 'be_server.py', '&']]
+#
+# run_frontend_srver = [['screen', '-S', 'backend'],
+#                       ['cd', 'frontend/'],
+#                       ['python3', 'fe_server.py', '&']]
 
-run_frontend_srver = [['screen', '-S', 'backend'],
-                      ['cd', 'frontend/'],
-                      ['python3', 'fe_server.py', '&']]
+run_backend_srver = ['python3', 'be_server.py', '&']
+run_frontend_srver = ['python3', 'fe_server.py', '&']
+
 if __name__ == '__main__':
-    for command in run_backend_srver:
-        subprocess.check_output(command)
-
-    for command in run_frontend_srver:
-        subprocess.check_output(command)
+    subprocess.check_output(run_backend_srver)
+    subprocess.check_output(run_frontend_srver)
