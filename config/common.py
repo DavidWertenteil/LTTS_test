5d11568de8b181e3ecd3bdd38e2e9f4ea10a9f88
noa5 <noa5por@gmail.com>
2018-05-06 12:34:01 +0300

    idle page

--- config/common.py
+++ config/common.py
@@ -1,4 +1,4 @@
-ORDER_FIELDS = ["cibus_number", "name", "main_dish", "side_dish_no.1", "side_dish_no.2"]
+ORDER_FIELDS = ["מס'_סיבוס", "שם", "מנה_עיקרית", "תוספת_1", "תוספת_2"]
 
 MAX_TIME_FOR_ORDERING = 15
 
