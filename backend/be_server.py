507d0259ea4ec2dccfa062ab34d4aab5c94ad757
dwertent <davhadassah@gmail.com>
2018-05-06 18:37:38 +0300

    Added root directory

--- backend/be_server.py
+++ backend/be_server.py
@@ -88,12 +88,12 @@ def send_email():
 
 
 if __name__ == '__main__':
-    # set_order()
-    # send_email()
-    # empty_orders_dir()
-    schedule.every().day.at("17:24").do(set_order)
-    schedule.every().day.at("17:25").do(send_email)
-    schedule.every().day.at("17:26").do(empty_orders_dir)
+    set_order()
+    send_email()
+    empty_orders_dir()
+    # schedule.every().day.at("17:24").do(set_order)
+    # schedule.every().day.at("17:25").do(send_email)
+    # schedule.every().day.at("17:26").do(empty_orders_dir)
     #
     # schedule.every(2).minutes.do(set_order)
     #
