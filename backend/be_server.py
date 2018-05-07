b6810682cbb417c01a87d13705fc3f4aa6624b96
dwertent <davhadassah@gmail.com>
2018-05-07 09:44:27 +0300

    Added root directory

--- backend/be_server.py
+++ backend/be_server.py
@@ -13,7 +13,7 @@ from config.ps import ps
 import csv
 from config.common import ORDER_FIELDS
 
-path_to_order_files = '../frontend/order_files/'
+path_to_order_files = 'frontend/order_files/'
 path_to_send_to_chef_files = 'backend/send_to_chef_files/'
 sender_email_address = 'LttsLunch@gmail.com'
 sender_email_password = ps
